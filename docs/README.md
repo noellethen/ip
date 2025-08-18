94dab3e5f2b5180ebeff08508d90ef7160d64e82
noellethen <nehtelleon@gmail.com>
2025-08-18 23:52:34 +0800

    Rename chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Poodle User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Poodle.java
@@ -0,0 +1,16 @@
+public class Poodle {
+    public static void main(String[] args) {
+        String logo =
+                """
+                                       ╔╦╗
+                                       ║║║
+                            ╔══╦══╦══╦═╝║║╔══╗
+                            ║╔╗║╔╗║╔╗║╔╗║║║║═╣
+                            ║╚╝║╚╝║╚╝║╚╝║╚╣║═╣
+                            ║╔═╩══╩══╩══╩═╩══╝
+                            ║║
+                            ╚╝
+                        """;
+        System.out.println("   Hello from\n\n" + logo);
+    }
+}
