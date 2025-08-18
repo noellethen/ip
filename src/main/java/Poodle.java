d089ae4b479b3042f6993e25851610bcfc4c4a2a
noellethen <nehtelleon@gmail.com>
2025-08-19 00:02:12 +0800

    Implement greet & exit

--- src/main/java/Poodle.java
+++ src/main/java/Poodle.java
@@ -12,5 +12,7 @@ public class Poodle {
                             ╚╝
                         """;
         System.out.println("   Hello from\n\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
