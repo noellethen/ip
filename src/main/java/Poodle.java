79cc111d40de1991db84605244d1cf33f9536319
noelle <>
2025-08-23 01:47:18 +0800

    Add simple to-do list feature

--- src/main/java/Poodle.java
+++ src/main/java/Poodle.java
@@ -1,9 +1,5 @@
 import java.util.Scanner;
 
-/**
- * Simple echo program that prints user input wrapped in a divider
- * until the user types "bye".
- */
 public class Poodle {
     private static final String DIVIDER = "--------------------------------------------";
     private static final String EXIT_COMMAND = "bye";
@@ -27,26 +23,61 @@ public class Poodle {
             awwwww bye :c hope to see you again soon! <3
             --------------------------------------------
             """;
+    private static final String LIST_COMMAND = "list";
+    private static final int MAX_TASKS = 100;
+    private static final String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
 
-    /**
-     * Reads lines from standard input and echoes them between dividers
-     * until the exit command is entered.
-     */
-    public static void echo() {
-        Scanner sc = new Scanner(System.in);
-
-        String input = sc.nextLine();
-        while (!EXIT_COMMAND.equals(input)) {
-            System.out.println(DIVIDER);
-            System.out.println(input);
-            System.out.println(DIVIDER);
+    private static void processInput(Scanner sc) {
+        String input;
+        while (true) {
             input = sc.nextLine();
+            if (EXIT_COMMAND.equals(input)) {
+                break;
+            }
+            if (input.isEmpty()) {
+                System.out.println("why empty >:c enter something!!");
+            } else if (input.equals(LIST_COMMAND)) {
+                showTasks();
+            } else {
+                addTask(input);
+            }
         }
+    }
+
+    private static void addTask(String input) {
+        printDivider();
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = input;
+            taskCount++;
+            System.out.println("yay :D added: " + input);
+        } else {
+            System.out.println("you have too many tasks >< pls clear first");
+        }
+        printDivider();
+    }
+
+    private static void showTasks() {
+        printDivider();
+        if (taskCount == 0) {
+            System.out.println("no tasks to show :c add one?");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
+        printDivider();
+    }
+
+    private static void printDivider() {
+        System.out.println(DIVIDER);
     }
 
     public static void main(String[] args) {
         System.out.println(ENTRY_TEXT);
-        echo();
+        try (Scanner sc = new Scanner(System.in)) {
+            processInput(sc);
+        }
         System.out.println(EXIT_TEXT);
     }
 }
