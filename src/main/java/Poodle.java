41fc15fa2cf91f75982311e5f06c31b65b0ba69f
noelle <>
2025-08-23 00:38:44 +0800

    Personalise chatbot style

--- src/main/java/Poodle.java
+++ src/main/java/Poodle.java
@@ -2,17 +2,19 @@ public class Poodle {
     public static void main(String[] args) {
         String logo =
                 """
-                                       ╔╦╗
-                                       ║║║
-                            ╔══╦══╦══╦═╝║║╔══╗
-                            ║╔╗║╔╗║╔╗║╔╗║║║║═╣
-                            ║╚╝║╚╝║╚╝║╚╝║╚╣║═╣
-                            ║╔═╩══╩══╩══╩═╩══╝
-                            ║║
-                            ╚╝
+                                   ╔╦╗
+                                   ║║║
+                        ╔══╦══╦══╦═╝║║╔══╗
+                        ║╔╗║╔╗║╔╗║╔╗║║║║═╣
+                        ║╚╝║╚╝║╚╝║╚╝║╚╣║═╣
+                        ║╔═╩══╩══╩══╩═╩══╝
+                        ║║
+                        ╚╝
                         """;
-        System.out.println("   Hello from\n\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("--------------------------------------------");
+        System.out.println("hiiiiii from\n\n" + logo);
+        System.out.println("is there anything i can do for you?");
+        System.out.println("---------------------------------------------");
+        System.out.println("awwwwww bye :c hope to see you again soon! <3");
     }
 }
