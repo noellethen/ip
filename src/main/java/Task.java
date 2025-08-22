6cd679db2a2e2315e8222b2bb4c25e9d4963fe6a
noelle <>
2025-08-23 02:26:22 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private final String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
