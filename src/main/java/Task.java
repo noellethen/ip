d489f533228d7648e79a2d52f464213d7d7869d2
noelle <>
2025-08-30 23:08:35 +0800

    Refactor code
    Moved some code that is more applicable to Task.java from Poodle.java to Task.java for readability.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,16 @@
 public class Task {
     private final String description;
     protected boolean isDone;
+    private static int taskCount = 0;
+
+    private static final int MAX_TASKS = 100;
+    private static Task[] taskList = new Task[MAX_TASKS];
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        taskList[taskCount] = this;
+        taskCount++;
     }
 
     public void markAsDone() {
@@ -13,6 +19,14 @@ public class Task {
 
     public void unmarkAsDone() {
         this.isDone = false;
+    }
+
+    public static int getTaskCount() {
+        return taskCount;
+    }
+
+    public static Task[] getTaskList() {
+        return taskList;
     }
 
     public String toString() {
