63794ff8cb2c30c4ed680dd0b5f3da3dabec2179
noelle <nehtelleon@gmail.com>
2025-09-06 16:23:45 +0800

    Add PoodleException class to represent exceptions specific to Poodle

---
+++ src/main/java/PoodleException.java
@@ -0,0 +1,39 @@
+public class PoodleException extends RuntimeException {
+
+    public enum Code {
+        UNKNOWN_COMMAND,
+        MISSING_ARGUMENT,
+        WRONG_ARGUMENT,
+        OUT_OF_RANGE
+    }
+
+    public PoodleException(Code code, String message) {
+        super(message);
+    }
+
+    public static PoodleException unknownCommandException(String input) {
+        return new PoodleException(Code.UNKNOWN_COMMAND,
+                "sorry :c i don't know what you mean by " + input);
+    }
+
+    public static PoodleException missingArgumentException(String input) {
+        String message = switch (input) {
+            case "todo" -> "todo what?? input something after todo!";
+            case "deadline" -> "what has a deadline?? input something after deadline!";
+            case "event" -> "what event?? input something after event!";
+            case "mark", "unmark" -> "enter something like this: " + input + " 2";
+            default -> "missing arguments for command " + input;
+        };
+
+        return new PoodleException(Code.MISSING_ARGUMENT, message);
+    }
+
+    public static PoodleException wrongFormatException(String input) {
+        return new PoodleException(Code.WRONG_ARGUMENT, input);
+    }
+
+    public static PoodleException outOfRangeException() {
+        return new PoodleException(Code.OUT_OF_RANGE,
+                "which task is that? >< from 1-100 pls!");
+    }
+}
