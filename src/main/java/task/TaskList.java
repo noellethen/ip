5479fc70a4b016546cc4429f734d03a8d508d9ac
noelle <nehtelleon@gmail.com>
2025-09-21 00:21:19 +0800

    Add TaskList.java to deal with task modification operations

---
+++ src/main/java/task/TaskList.java
@@ -0,0 +1,78 @@
+package task;
+
+import exception.PoodleException;
+import parser.Parser;
+import storage.Storage;
+import ui.Ui;
+
+public class TaskList {
+
+    private final Ui ui;
+
+    public TaskList(Ui ui) {
+        this.ui = ui;
+    }
+
+    public void loadTasks() {
+        try {
+            Storage.loadTaskListFromFile();
+        } catch (PoodleException e) {
+            ui.printErrorMessage("i got an error! " + e.getMessage());
+        }
+    }
+
+    public void saveTasks() {
+        try {
+            Storage.saveTaskListToFile();
+        } catch (PoodleException e) {
+            ui.printErrorMessage("i got an error! " + e.getMessage());
+        }
+    }
+
+    public void handleMark(String firstWord, String input) {
+        int firstSpaceIndex = Parser.getFirstSpaceIndex(input);
+        if (firstSpaceIndex == -1) {
+            throw PoodleException.missingArgumentException(firstWord);
+        }
+
+        int taskNumber;
+        try {
+            taskNumber = Integer.parseInt(input.substring(firstSpaceIndex));
+        } catch (NumberFormatException e) {
+            throw PoodleException.missingArgumentException(firstWord);
+        }
+
+        int count = Task.getTaskCount();
+        if (taskNumber < 1 || taskNumber > count) {
+            throw PoodleException.outOfRangeException(count);
+        }
+
+        Task task = Task.getTaskList().get(taskNumber - 1);
+        if (firstWord.equals(Ui.MARK_COMMAND)) {
+            task.markAsDone();
+            ui.printTaskMarkedAsDone(task);
+        } else if (firstWord.equals(Ui.UNMARK_COMMAND)) {
+            task.unmarkAsDone();
+            ui.printTaskUnmarked(task);
+        }
+    }
+
+    public void deleteTask(String input) {
+        int firstSpaceIndex = Parser.getFirstSpaceIndex(input);
+
+        if (firstSpaceIndex == 0) {
+            throw PoodleException.missingArgumentException(Ui.DELETE_COMMAND);
+        }
+
+        int index = Integer.parseInt(input.substring(firstSpaceIndex)) - 1;
+
+        Task task = Task.getTaskList().get(index);
+        task.removeTask();
+        saveTasks();
+        ui.printTaskDeleted(task);
+    }
+
+    public void showTasks() {
+        ui.printTaskList();
+    }
+}
